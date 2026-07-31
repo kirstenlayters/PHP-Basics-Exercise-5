d25797fd0c2d1c963d6cb5626ed023cae89d4fde
kirstenlayters <kirsten.layters@gmail.com>
2026-07-31 15:44:04 +0200

    Update exercise 5.4.php

--- exercise 5.4.php
+++ exercise 5.4.php
@@ -1,9 +1,8 @@
 <?php
-// 1. DATABASE CONNECTION
 $host = 'localhost';
 $db   = 'PHP';
-$user = 'root';     // Change to your MySQL username
-$pass = 'Kirsten.L1404';         // Change to your MySQL password
+$user = 'root';
+$pass = 'Kirsten.L1404';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
@@ -19,10 +18,6 @@ try {
     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 
-
-// ---------------------------------------------------------
-// 2. CREATE (INSERT a new user)
-// ---------------------------------------------------------
 $name  = 'Alex';
 $email = 'alex@gmail.com';
 
@@ -35,10 +30,6 @@ $stmt->execute([
 
 echo "Created user with ID: " . $pdo->lastInsertId() . "<br>";
 
-
-// ---------------------------------------------------------
-// 3. READ (RETRIEVE all users)
-// ---------------------------------------------------------
 $sql = "SELECT * FROM users";
 $stmt = $pdo->query($sql);
 $users = $stmt->fetchAll();
@@ -48,10 +39,6 @@ foreach ($users as $u) {
     echo "ID: " . $u['User_ID'] . " | Name: " . $u['Name'] . " | Email: " . $u['Email'] . "<br>";
 }
 
-
-// ---------------------------------------------------------
-// 4. UPDATE (Change Vee's email for User_ID 10)
-// ---------------------------------------------------------
 $newEmail = 'vee.updated@gmail.com';
 $userIdToUpdate = 10;
 
@@ -64,10 +51,6 @@ $stmt->execute([
 
 echo "<br>Updated Email for User_ID " . $userIdToUpdate . "<br>";
 
-
-// ---------------------------------------------------------
-// 5. DELETE (Delete Sky using User_ID 11)
-// ---------------------------------------------------------
 $userIdToDelete = 11;
 
 $sql = "DELETE FROM users WHERE User_ID = :id";
